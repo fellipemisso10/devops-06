9e736cca7f4cfce8bf5955562e7028381740a96a
Luiz Fellipe Misso Valota <1902344@SALAS.AULAS>
2019-09-30 21:19:21 -0300

    Versão 2.0

---
+++ src/primos.py
@@ -0,0 +1,12 @@
+numero = int(input('Digite um número'))
+let qtdPrimo = 0
+for n in range (1, num +1){
+    if (numero % i == 0){
+        qtdPrimo += 1
+    }
+    else{
+        numero++
+    }
+}    
+
+return numero
